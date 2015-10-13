cf74b894c48f6c707870af09e80153fd1ca9a22a
Lukas Boersma <lukas.boersma@gmail.com>
2015-10-13 16:26:21 +0200

    Initial Commit

---
+++ WindowsBallz/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using CoreBallz;
+
+namespace Ballz.Windows
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Ballz())
+                game.Run();
+        }
+    }
+#endif
+}
