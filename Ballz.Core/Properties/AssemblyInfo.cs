19e4e8440556d4ac938a5bcd42d949182867b670
Marcus Voelker <marcus@upvoid.com>
2015-10-20 20:18:25 +0200

    GUID fun for the whole family. I still blame Lukas

--- Ballz.Core/Properties/AssemblyInfo.cs
+++ Ballz.Core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("{D5FCA137-178B-486B-8677-5BF65FF1F5FE}")]
+[assembly: Guid("{249B38E3-8874-4903-9AC8-7C2FE06F1B56}")]
 
 // Version information for an assembly consists of the following four values:
 //
