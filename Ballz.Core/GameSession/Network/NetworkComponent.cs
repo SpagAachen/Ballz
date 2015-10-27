9a916d9f8cb299b9f12510304585dc0d016211d9
Marcus Voelker <marcus@upvoid.com>
2015-10-27 18:24:18 +0100

    Missing files from last commit

--- Ballz.Core/GameSession/Network/NetworkComponent.cs
+++ Ballz.Core/GameSession/Network/NetworkComponent.cs
@@ -1,27 +1,24 @@
-﻿namespace Ballz.GameSession.Network
+﻿using Ballz.Messages;
+using Microsoft.Xna.Framework;
+
+namespace Ballz.GameSession.Network
 {
-    using System;
-    using Microsoft.Xna.Framework;
-    using Messages;
-
-
     /// <summary>
-    /// Network control Takes care of all network related stuff.
+    ///     Network control Takes care of all network related stuff.
     /// </summary>
     public class NetworkControl : GameComponent
-   {
-      public NetworkControl (Game _game) : base (_game)
-      {
-      }
+    {
+        public NetworkControl(Game _game) : base(_game)
+        {
+        }
 
-      public void update (GameTime _time)
-      {
-      }
+        public void update(GameTime _time)
+        {
+        }
 
-      public void handleMessage (object _sender, Message _message)
-      {
-         //TODO: handle Messages
-      }
-   }
+        public void handleMessage(object _sender, Message _message)
+        {
+            //TODO: handle Messages
+        }
+    }
 }
-
