2f26fdecccafcef582488e57a9e79876a05cd44a
Marc Seebold <marc.seebold@rwth-aachen.de>
2015-11-13 11:14:15 +0100

    Menu: Network entry

--- Ballz.Core/Menu/GameMenu.cs
+++ Ballz.Core/Menu/GameMenu.cs
@@ -15,13 +15,28 @@ namespace Ballz.Menu
         {
             get
             {
-                //just some plain dummy menu entries
+                // options menu
                 var optionsMenu = new GameMenu("Options", true);
                 optionsMenu.Items.AddLast(new GameMenu("Not Implemented", false));
 
+                // multiplayer menu
+                var networkMenu = new GameMenu("Multiplayer", true);
+                // - connect to server
+                var networkConnectToMenu = new GameMenu("Connect to", true);
+                networkConnectToMenu.Items.AddLast(new GameMenu("Not Implemented", false));
+                // - start server
+                var networkServerMenu = new GameMenu("Start server", true);
+                networkServerMenu.Items.AddLast(new GameMenu("Not Implemented", false));
+                // - add items
+                networkMenu.Items.AddLast(networkConnectToMenu);
+                networkMenu.Items.AddLast(networkServerMenu);
+                networkMenu.Items.AddLast(new GameMenu("Back", true));
+
+                // main menu
                 var mainMenu = new GameMenu("Main Menu");
                 mainMenu.Items.AddLast(new GameMenu("Play", true));
                 mainMenu.Items.AddLast(optionsMenu);
+                mainMenu.Items.AddLast(networkMenu);
                 mainMenu.Items.AddLast(new GameMenu("Quit", true));
 
                 mainMenu.SelectedItem = mainMenu.Items.First;
