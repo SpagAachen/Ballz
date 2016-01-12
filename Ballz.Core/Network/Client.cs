SpagAachen/Ballz

commit 703ef5e5adfb18eb0251649128c14da1dc88e500

more network

Ballz.Core/Network/Client.cs
@@ -52,9 +52,27 @@
 					ourE.Velocity = e.Velocity;
 					ourE.Rotation = e.Rotation;
 				}
+                return;
 			}
-			else
-				Console.WriteLine("Unknown object received: " + data.ToString());
+            // network message
+            var netMsg = data as NetworkMessage;
+            if (netMsg != null)
+            {
+                switch (netMsg.Kind)
+                {
+                    case NetworkMessage.MessageType.GameStarted:
+                        Ballz.The().Logic.startGame();
+                        break;
+                    default:
+                        Console.WriteLine("Unknown netMsg received: " + netMsg.Kind.ToString());
+                        break;
+                }
+                return;
+            }
+            if (data)
+			    Console.WriteLine("Unknown object received: " + data.ToString());
+            else
+                Console.WriteLine("Empty data");
 		}
 
 		public void HandleInputMessage(InputMessage message)

Ballz.Core/Ballz.cs
@@ -39,6 +39,8 @@ namespace Ballz
 
         public Settings.ProgrammSettings GameSettings;
 
+        public Label NetworkLobbyConnectedClients = new Label ("test", true);
+
         public Composite MainMenu;
 
         public Camera Camera;
@@ -195,12 +197,23 @@ namespace Ballz
 			networkConnectToLabel.OnSelect += () => Network.ConnectToServer (networkHostInput.Value, 13337);
 			networkConnectToMenu.AddItem (networkConnectToLabel);
             // - start server
-            var networkServerMenu = new Label("Start server", true);
+            var networkServerMenu = new Composite("Start Server", true);
+
 			networkServerMenu.OnSelect += () => 
 			{
-				Network.StartServer (13337); //TODO: port input
-				Logic.startGame ();
+				Network.StartServer(13337); //TODO: port input
 			};
+            // network lobby
+            networkServerMenu.AddItem(NetworkLobbyConnectedClients);
+            var networkServerMenuStartGame = new Label ("Start Game", true);
+            networkServerMenuStartGame.OnSelect += () => 
+                {
+                    Logic.startGame();
+                    Network.GameStarted();
+                };
+            networkServerMenu.AddItem(networkServerMenuStartGame);
+            //TODO: abort button - close server etc.
+
             // - add items
             networkMenu.AddItem(networkConnectToMenu);
             networkMenu.AddItem(networkServerMenu);

Ballz.Core/Logic/LogicControl.cs
@@ -84,7 +84,8 @@ namespace Ballz.Logic
 			switch (msg.Kind)
 			{
 			case NetworkMessage.MessageType.ConnectedToServer:
-				startGame ();
+				//TODO: show lobby!!!!<<<<<<<<<<<<<<<<<<<<<<<<<
+                    //startGame ();
 				break;
 			}
 		}

Ballz.Core/Menu/Item.cs
@@ -16,7 +16,7 @@ namespace Ballz.Menu
         }
 
         //The name
-        public string Name { get; }
+        public string Name { get; set; }
 
         public abstract IReadOnlyList<Item> Items { get; }
 

Ballz.Core/Messages/NetworkMessage.cs
@@ -8,7 +8,8 @@
             ConnectingToServer,
             ConnectedToServer,
             ServerStarted,
-            NewClient
+            NewClient,
+            GameStarted
         }
 
         public NetworkMessage(MessageType type) : base(Message.MessageType.NetworkMessage)

Ballz.Core/Network/Network.cs
@@ -10,6 +10,7 @@
     public class Network : GameComponent
     {
         public enum StateT { None, Client, Server };
+        public enum GameStateT { None, InLobby, InGame };
         private Server server;
         private Client client;
         public event EventHandler<Message> Message;
@@ -18,6 +19,7 @@
         /// The state of the game: Unconnected/None, Client or Server
         /// </summary>
         public StateT State { get; private set; } = StateT.None;
+        public GameStateT GameState { get;  set; } = GameStateT.None;
 
         public void RaiseMessageEvent(NetworkMessage.MessageType msg)
         {
@@ -33,6 +35,14 @@
             server.Listen(port);
             RaiseMessageEvent(NetworkMessage.MessageType.ServerStarted);
             //TODO: Implement
+        }
+
+        public void GameStarted()
+        {
+            if (State == StateT.Server)
+            {
+                server.GameStarted();
+            }
         }
 
         public void ConnectToServer(string hostname, int port)

Ballz.Core/Network/Server.cs
@@ -9,22 +9,38 @@
 
 	class Server
     {
-		private int test = 0;
-
         private static int nextId = 1;
         TcpListener listener = null;
         private readonly Network network = null;
         private readonly List<Connection> connections = new List<Connection>();
+
 
         public Server(Network net)
         {
             network = net;
         }
 
+        public void GameStarted()
+        {
+            Broadcast(new NetworkMessage(NetworkMessage.MessageType.GameStarted));
+        }
+
         public void Listen(int port)
         {
             listener = new TcpListener(IPAddress.Any, port);
             listener.Start();
+            // start lobby first
+            network.GameState = Network.GameStateT.InLobby;
+            updateLobbyList();
+        }
+
+        public void updateLobbyList()
+        {
+            Ballz.The().NetworkLobbyConnectedClients.Name = "Myself";
+            foreach(var c in connections)
+            {
+                Ballz.The().NetworkLobbyConnectedClients.Name += ", " + c.Id;
+            }
         }
 
         public void Update(GameTime time)
@@ -32,9 +48,19 @@
             // new clients
             if (listener.Pending())
             {
-                var client = listener.AcceptTcpClient();
-                connections.Add(new Connection(client, nextId++));
-                network.RaiseMessageEvent(NetworkMessage.MessageType.NewClient);
+                if (network.GameState == Network.GameStateT.InLobby)
+                {
+                    // add new player in lobby
+                    var client = listener.AcceptTcpClient();
+                    connections.Add(new Connection(client, nextId++));
+                    network.RaiseMessageEvent(NetworkMessage.MessageType.NewClient);
+                    // update lobby list
+                    updateLobbyList();
+                }
+                else
+                {
+                    //TODO: Re-add Players that lost connection
+                }
             }
             // receive data
             foreach (var c in connections)
@@ -48,8 +74,7 @@
 
 			// TEST
 			{
-				test++;
-				Broadcast(Ballz.The().World.Entities);
+				//Broadcast(Ballz.The().World.Entities);
 			}
             //TODO: Implement
         }

commit f70482685f9047d256fb9ac46d62312bca329073

fixed the build
as it was broken since
commit 703ef5e5adfb18eb0251649128c14da1dc88e500

Ballz.Core/Network/Client.cs
@@ -69,7 +69,7 @@
                 }
                 return;
             }
-            if (data)
+            if (data != null)
 			    Console.WriteLine("Unknown object received: " + data.ToString());
             else
                 Console.WriteLine("Empty data");