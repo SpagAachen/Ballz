d67cf08017ef04826006b4296a1f5ff595035f03
Nicht der Marc! <Nicht der Marc!@rwth-aachen.de>
2016-09-07 21:37:31 +0200

    Allow fancy interpolation for GraphicsEvents.

--- Ballz.Core/GameSession/World/GraphicsEvent.cs
+++ Ballz.Core/GameSession/World/GraphicsEvent.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MathFloat;
 
 namespace Ballz.GameSession.World
 {
@@ -21,53 +22,99 @@ namespace Ballz.GameSession.World
 
     public class GenericGraphicsEffect : GraphicsEvent
     {
+        // Where is the only additional keyframe? \in 0..1
+        private float _midFactor = 0.5f;
+        public float   MidFactor     
+        { 
+            get
+            {
+                return _midFactor;
+            }
+            set
+            {
+                if(value <= 0 || value >= 1)
+                    throw new ArgumentOutOfRangeException();
+                
+                _midFactor = value;
+            } 
+        }
+
         public Vector2 PositionStart { get; set; }
-        public Vector2 PositionMid { get; set; }
-        public Vector2 PositionEnd { get; set; }
+        public Vector2 PositionMid   { get; set; }
+        public Vector2 PositionEnd   { get; set; }
 
-        public float RotationStart { get; set; } = 0;
-        public float RotationMid { get; set; } = 0;
-        public float RotationEnd { get; set; } = 0;
+        public float RotationStart   { get; set; } = 0;
+        public float RotationMid     { get; set; } = 0;
+        public float RotationEnd     { get; set; } = 0;
 
-        public float OpacityStart { get; set; } = 1;
-        public float OpacityMid { get; set; } = 1;
-        public float OpacityEnd { get; set; } = 1;
+        public float OpacityStart    { get; set; } = 1;
+        public float OpacityMid      { get; set; } = 1;
+        public float OpacityEnd      { get; set; } = 1;
 
-        public float ScaleStart { get; set; } = 1;
-        public float ScaleMid { get; set; } = 1;
-        public float ScaleEnd { get; set; } = 1;
+        public float ScaleStart      { get; set; } = 1;
+        public float ScaleMid        { get; set; } = 1;
+        public float ScaleEnd        { get; set; } = 1;
 
         public Vector2 Position(float gameTime)
         {
-            return PositionStart;
+            var progress = GetProgress(gameTime);
+            
+            if (progress < MidFactor)
+                return Vector2.Lerp(PositionStart, PositionMid, progress / MidFactor);
+
+            // progress >= MidFactor
+            return Vector2.Lerp(PositionMid, PositionEnd, (progress-MidFactor) / (1 - MidFactor));
+        }
+
+        // Why the hell is there no float.lerp(...) ?
+        public static float Mix(float val0, float val1, float a)
+        {
+            return val0 + (val1 - val0) * a;
+        }
+
+        // Helper function
+        private float interpolateFloat(float valStart, float valMid, float valEnd, float gameTime)
+        {
+            var progress = GetProgress(gameTime);
+
+            if (progress < MidFactor)
+                return Mix(valStart, valMid, progress / MidFactor);
+
+            // progress >= MidFactor
+            return Mix(valMid, valEnd, (progress-MidFactor) / (1 - MidFactor));
         }
 
         public float Rotation(float gameTime)
         {
-            return RotationStart;
+            return interpolateFloat(RotationStart, RotationMid, RotationEnd, gameTime);
         }
 
         public float Opacity(float gameTime)
         {
-            return OpacityStart;
+            return interpolateFloat(OpacityStart, OpacityMid, OpacityEnd, gameTime);
         }
 
         public float Scale(float gameTime)
         {
-            return ScaleStart;
+            return interpolateFloat(ScaleStart, ScaleMid, ScaleEnd, gameTime);
         }
         
         public string SpriteName { get; set; }
 
-        public static GenericGraphicsEffect CreateExplosion(float gameTime, Vector2 position, float rotation, float scale = 1)
+        public static GenericGraphicsEffect CreateExplosion(float gameTime, Vector2 position, float rotation, float scaleStart = 1, float scaleEnd = 1.5f)
         {
             return new GenericGraphicsEffect
             {
                 Start = gameTime,
                 Duration = 0.2f,
                 PositionStart = position,
+                PositionMid = position,
+                PositionEnd = position,
                 RotationStart = rotation,
-                ScaleStart = scale,
+                RotationMid = rotation,
+                RotationEnd = rotation,
+                ScaleStart = scaleStart,
+                ScaleEnd = scaleEnd,
                 SpriteName = "Explosion"
             };
         }
@@ -79,7 +126,11 @@ namespace Ballz.GameSession.World
                 Start = gameTime,
                 Duration = 0.1f,
                 PositionStart = position,
+                PositionMid = position,
+                PositionEnd = position,
                 RotationStart = rotation,
+                RotationMid = rotation,
+                RotationEnd = rotation,
                 SpriteName = "Muzzle"
             };
         }
