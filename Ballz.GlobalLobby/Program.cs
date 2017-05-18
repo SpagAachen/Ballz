cf00f9bbd6f5b2f9f3c63ce788a24e9667a38252
Lukas Boersma <lukas.boersma@rwth-aachen.de>
2017-05-18 21:33:12 +0200

    Remove test entries from global lobby server

--- Ballz.GlobalLobby/Program.cs
+++ Ballz.GlobalLobby/Program.cs
@@ -16,27 +16,6 @@ namespace Ballz.GlobalLobby
             var app = new App();
             var gameList = new GameList();
 
-            gameList.AddGame(new Lobby.FullGameInfo
-            {
-                Name = "Hey",
-                HostAddress = "Bla",
-                HostPort = 123,
-                IsPrivate = false,
-                PublicId = "sald7a8h",
-                LastKeepAlive = DateTime.Now,
-                Secret = "sa87hdas9asn98"
-            });
-            gameList.AddGame(new Lobby.FullGameInfo
-            {
-                Name = "sdfds",
-                HostAddress = "322",
-                HostPort = 456,
-                IsPrivate = false,
-                PublicId = "vbgf",
-                LastKeepAlive = DateTime.MaxValue,
-                Secret = "6jhdf"
-            });
-
             app.AddView(@"^/game/list/$", ctx => gameList.RequestGetGames(ctx));
             app.AddView(@"^/game/add/$", ctx => gameList.RequestAddGame(ctx));
             app.AddView(@"^/game/remove/$", ctx => gameList.RequestRemoveGame(ctx));
