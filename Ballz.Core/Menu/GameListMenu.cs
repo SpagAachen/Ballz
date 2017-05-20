d4894d9cab720c4f57710f08931c35737a3867ab
Lukas Boersma <lukas.boersma@rwth-aachen.de>
2017-05-20 02:46:36 +0200

    Multiplayer lobby GUI

--- Ballz.Core/Menu/GameListMenu.cs
+++ Ballz.Core/Menu/GameListMenu.cs
@@ -6,14 +6,21 @@ using System.Threading.Tasks;
 
 using Ballz.Lobby;
 using GeonBit.UI.Entities;
+using Microsoft.Xna.Framework;
 
 namespace Ballz
 {
     class GameListMenu : Gui.MenuPanel
     {
         LobbyClient Lobby = null;
+
+        Label StatusLabel = new Label("Updating Game List");
+        SelectList GameList = new SelectList(new Vector2(0, 350));
+        Button JoinButton = new Gui.MenuButton("Join", anchor: Anchor.BottomRight, size: new Vector2(0.45f, -1));
+
         public GameListMenu() : base("Join Online Game")
         {
+            Size = new Vector2(800, 0);
             Open += (s,e) =>
             {
                 Lobby = new LobbyClient();
@@ -25,8 +32,13 @@ namespace Ballz
                 Lobby = null;
             };
 
-            AddItem(new Label("Loading Games..."));
-            AddItem(new Gui.BackButton());
+            AddItem(StatusLabel);
+            AddItem(GameList);
+            var panel = new Panel(new Vector2(0, 150));
+            JoinButton.Disabled = true;
+            panel.AddChild(JoinButton);
+            panel.AddChild(new Gui.BackButton(Anchor.BottomLeft, new Vector2(0.45f, -1)));
+            AddItem(panel);
         }
 
         string SelectedGameId = "";
@@ -39,19 +51,38 @@ namespace Ballz
 
         public void UpdateGameList(object sender, PublicGameInfo[] games)
         {
-            Members.Clear();
+            StatusLabel.Text = "Successfully updated game list.";
 
-            foreach(PublicGameInfo g in games)
+            var selection = GameList.SelectedValue;
+            var scrollPos = GameList.ScrollPosition;
+            var isFocussed = GameList.IsFocused;
+
+            GameList.ClearItems();
+            if (games.Length == 0)
             {
-                //AddItem(new Label(g.Name,));
+                GameList.AddItem("[No games found]");
+            }
+            else
+            {
+                foreach (PublicGameInfo g in games)
+                {
+                    GameList.AddItem(g.Name);
+                }
             }
 
-            if(games.Length == 0)
+            try
             {
-                //AddItem(new Gui.Label("No games found."));
+                GameList.ScrollPosition = scrollPos;
+                if (isFocussed)
+                {
+                    GeonBit.UI.UserInterface.ActiveEntity = GameList;
+                }
+                GameList.SelectedValue = selection;
             }
-
-            AddItem(new Gui.BackButton());
+            catch(System.Exception e)
+            {
+                GameList.SelectedIndex = 0;
+            }
         }
     }
 }
--- Ballz.Core/Menu/HostGameMenu.cs
+++ Ballz.Core/Menu/HostGameMenu.cs
@@ -5,26 +5,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Ballz.Gui;
+using Microsoft.Xna.Framework;
+using GeonBit.UI.DataTypes;
+
 namespace Ballz
 {
     class HostGameMenu : Gui.MenuPanel
     {
+        Label ErrorLabel = new Label("");
+        TextInput GameName = new TextInput(false);
+        DropDown ModeSelect = new DropDown(new Microsoft.Xna.Framework.Vector2(0, -1));
+        CheckBox TurnBased = new CheckBox("Turn-based");
+
         public HostGameMenu(bool isMultiplayer) : base(isMultiplayer ? "Host Game" : "Singleplayer Game")
         {
+            Skin = PanelSkin.Default;
+
             if (isMultiplayer)
             {
                 //AddItem(new Gui.InputBox("Public Name", true));
             }
-            AddItem(new Label("Map: Desert"));
-            AddItem(new Label("Turn-based: Yes"));
+
+
+            AddItem(new Label("Game Name:"));
+            AddItem(GameName);
+
+            AddItem(new Label("Map:"));
+            // TODO: Map Select
+            ModeSelect.AddItem("TestWorld2");
+            ModeSelect.AddItem("Procedural");
+            ModeSelect.AddItem("Desert");
+            AddItem(ModeSelect);
+            AddItem(TurnBased);
+
+            ErrorLabel.SetStyleProperty("FillColor", new StyleProperty(Color.Red));
+            AddItem(ErrorLabel);
+
             if (isMultiplayer)
             {
-                var startGameLabel = new Label("Open Game");
-                //startGameLabel.OnSelect += () =>
-                //{
-                //    Ballz.The().Logic.OpenMenu(new LobbyMenu(true, "Testgame!!!!!!!!!!!!!!!!", false));
-                //};
-                AddItem(startGameLabel);
+                var startGameButton = new MenuButton("Open Game", () =>
+                {
+                    var name = GameName.Value;
+                    if(String.IsNullOrWhiteSpace(name))
+                    {
+                        ErrorLabel.Text = "Invalid Game Name!";
+                        return;
+                    }
+                    Ballz.The().Logic.OpenMenu(new LobbyMenu(true, GameName.Value, false));
+                });
+
+                AddItem(startGameButton);
             }
             else
             {
